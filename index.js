987bb322eaa9c1ae9109246da5b171fb240c73fa
Your Name <shubhampathak1996@gmail.com>
2023-07-14 00:11:30 +0530

    learn_full

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 const express = require("express");
+require("./services/passport")
+const mongoose = require("mongoose")
+const authRoutes = require("./routes/authRoutes")
 const app = express()
+
+
+
+var db = "mongodb://192.168.1.9:27017/emaily-dev";
+mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const conSuccess = mongoose.connection
+conSuccess.once('open', _ => {
+  console.log('Database connected:', db)
+})
+
+
+
+
+
+authRoutes(app)
+
+
+
+
 const PORT = process.env.PORT || 5000
-
-app.get('/',(req,res)=>{
-    res.send({hi: "There"})
-})
-app.get('/greetings',(req,res)=>{
-    res.send({hi: 'Mere bete gaand main jao'})
-})
-
 app.listen(PORT );
