a4ca8cbbdc8f8041db64607051a28e02542ad366
shubham pathak <shubhampathak1996@gmail.com>
2023-07-28 17:55:27 +0530

    redux_setup

--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,21 @@
+import materializeCSS from 'materialize-css/dist/css/materialize.min.css';
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
+
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import reduxThunk from 'redux-thunk';
+
 import App from './components/App';
 import reducers from './reducers';
 
-const store = createStore(reducers, {}, applyMiddleware());
+const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
-ReactDOM.render(
+const el = document.getElementById('root');
+const root = ReactDOM.createRoot(el);
+
+root.render(
   <Provider store={store}>
     <App />
-  </Provider>,
-
-  document.querySelector('#root')
+  </Provider>
 );
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,20 +1,20 @@
 const passport = require('passport');
 
-module.exports = app => {
+module.exports = (app) => {
   app.get(
     '/auth/google',
     passport.authenticate('google', {
-      scope: ['profile', 'email']
+      scope: ['profile', 'email'],
     })
   );
 
   app.get('/auth/google/callback', passport.authenticate('google'));
-
+  // logout
   app.get('/api/logout', (req, res) => {
     req.logout();
     res.send(req.user);
   });
-
+  // current user
   app.get('/api/current_user', (req, res) => {
     res.send(req.user);
   });
